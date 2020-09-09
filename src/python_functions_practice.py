de4894fb76bbc6e04916a01542d89de9bec26d26
DevSharpknife <davidjamesmurphy@gmail.com>
2020-09-09 16:04:18 +0100

    tests 7 and 8 done

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -13,6 +13,11 @@ def multiply(num_1, num_2):
 def divide(num_1, num_2):
     return num_1 / num_2
 
-def test_length_of_string():
+def length_of_string(test_string):
     return (len("A string of length 21"))  
-      
+
+def join_string(string_1, string_2):
+    return f"{string_1}{string_2}"
+
+def add_string_as_number(str_1, str_2):
+    return int(str_1) + int(str_2)
